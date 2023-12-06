86acfc964e5831f1f86ee29a39e4898c9133c232
Михаил <04nightmare86@gmail.com>
2023-12-06 20:14:20 +0900

    Add connection to database

---
+++ modules/database.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt6.QtSql import QSqlDatabase
+from PyQt6.QtWidgets import QMessageBox
+
+
+class Database:
+    def __init__(self):
+        self.con = QSqlDatabase.addDatabase("QSQLITE")
+        self.con.setDatabaseName("./database/ias.db")
+        if not self.con.open():
+            QMessageBox.critical(
+                None, "Ошибка базы данных",
+                "Database Error: %s" % self.con.lastError().databaseText(),
+            )
+
