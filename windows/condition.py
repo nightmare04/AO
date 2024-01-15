b1742d854544301bcb2da37a64d65ba5291ef91c
Mikhail Malinichev <04nightmare86@gmail.com>
2024-01-15 13:38:48 +0900

    bump

--- windows/condition.py
+++ windows/condition.py
@@ -49,6 +49,7 @@ class Condition(QtWidgets.QWidget):
 class PlaneCondition(QtWidgets.QWidget):
     def __init__(self, plane: PlaneM):
         super().__init__()
+        self.new_form = None
         self.plane = plane
         self.resize(700, 700)
         self.setWindowTitle(f'Самолет №{self.plane.tail_number}')
@@ -67,7 +68,7 @@ class PlaneCondition(QtWidgets.QWidget):
         self.defect_btn_layout = QtWidgets.QHBoxLayout()
         self.defect_layout.addLayout(self.defect_btn_layout)
         self.defect_add_btn = QtWidgets.QPushButton('Добавить')
-        self.defect_add_btn.clicked.connect(self.add_defect)
+        self.defect_add_btn.clicked.connect(self.open_add_defect)
         self.defect_btn_layout.addWidget(self.defect_add_btn)
         self.defect_table.setColumnCount(5)
         self.defect_table.setHorizontalHeaderLabels(['Наименование', 'Система', 'Номер', 'Изменить', 'Удалить'])
@@ -79,7 +80,7 @@ class PlaneCondition(QtWidgets.QWidget):
         self.removed_groupbox.setLayout(self.removed_layout)
         self.agr_layout.addWidget(self.removed_groupbox)
         self.removed_add_btn = QtWidgets.QPushButton('Добавить')
-        self.removed_add_btn.clicked.connect(self.add_removed)
+        self.removed_add_btn.clicked.connect(self.open_add_removed)
         self.removed_groupbox.layout().addWidget(self.removed_table)
         self.removed_layout.addLayout(self.removed_btn_layout)
         self.removed_btn_layout.addWidget(self.removed_add_btn)
@@ -87,11 +88,12 @@ class PlaneCondition(QtWidgets.QWidget):
         self.removed_table.setHorizontalHeaderLabels(['Наименование', 'Система', 'Номер',
                                                       'Куда снято', 'Изменить', 'Удалить'])
 
-    def add_removed(self):
+    def open_add_removed(self):
         pass
 
-    def add_defect(self):
-        pass
+    def open_add_defect(self):
+        self.new_form = AddDefect(self.plane)
+        self.new_form.show()
 
 
 class AddDefect(QtWidgets.QWidget):
@@ -105,34 +107,54 @@ class AddDefect(QtWidgets.QWidget):
         self.main_layout.addLayout(self.grid_layout)
         self.setLayout(self.main_layout)
 
-        self.subunit_label = QtWidgets.QLabel('Специальность:')
+        self.subunit_name_label = QtWidgets.QLabel('Специальность:')
         self.subunit_name_edit = QtWidgets.QComboBox()
-        subunits_list = list(SubunitM.select(SubunitM.name).where(SystemM.plane_type == self.plane.plane_type))
+        subunits_list = list(SubunitM.select(SubunitM.name).where(SubunitM.plane_type == self.plane.plane_type))
         subunits_name = map(lambda q: q.name, subunits_list)
-        self.system_name_edit.addItems(subunits_name)
-        self.main_layout.addWidget(self.system_name_label, 1, 0)
-        self.main_layout.addWidget(self.system.name_edit, 1, 1)
+        self.subunit_name_edit.addItems(subunits_name)
+        self.grid_layout.addWidget(self.subunit_name_label, 0, 0)
+        self.grid_layout.addWidget(self.subunit_name_edit, 0, 1)
+        self.subunit_name_edit.currentTextChanged.connect(self.fill_systems)
 
         self.system_name_label = QtWidgets.QLabel('Система:')
         self.system_name_edit = QtWidgets.QComboBox()
-        systems_list = list(SystemM.select(SystemM.name).where(SystemM.plane_type == self.plane.plane_type))
+        systems_list = list(SystemM.select(SystemM.name).
+                            where(SystemM.plane_type == self.plane.plane_type,
+                                  SystemM.subunit_type == SubunitM.get(SubunitM.name ==
+                                                                       self.subunit_name_edit.currentText()).id))
         systems_name = map(lambda q: q.name, systems_list)
         self.system_name_edit.addItems(systems_name)
-        self.main_layout.addWidget(self.system_name_label, 1, 0)
-        self.main_layout.addWidget(self.system.name_edit, 1, 1)
+        self.grid_layout.addWidget(self.system_name_label, 1, 0)
+        self.grid_layout.addWidget(self.system_name_edit, 1, 1)
+        self.system_name_edit.currentTextChanged.connect(self.fill_agr)
 
-        self.type_label = QtWidgets.QLabel('Выберите блок / агрегат:')
-        self.type_select = QtWidgets.QComboBox()
-        agrs_list = list(AgregateM.select().where(
-            AgregateM.id_system == AgregateM.get(AgregateM.name == self.system_name_edit.currentText())))
+        self.agr_label = QtWidgets.QLabel('Выберите блок / агрегат:')
+        self.agr_select = QtWidgets.QComboBox()
+        agrs_list = list(AgregateM.select().where(AgregateM.id_system == SystemM.get(SystemM.name == self.system_name_edit.currentText()).id))
         agrs_name = map(lambda q: q.name, agrs_list)
-        self.type_select.addItems(agrs_name)
-        self.main_layout.addWidget(self.type_label, 2, 0)
-        self.main_layout.addWidget(self.type_select, 2, 1)
+        self.agr_select.addItems(agrs_name)
+        self.grid_layout.addWidget(self.agr_label, 2, 0)
+        self.grid_layout.addWidget(self.agr_select, 2, 1)
 
         self.add_btn = QtWidgets.QPushButton('Добавить')
         self.add_btn.clicked.connect(self.add_defect)
-        self.main_layout.addWidget(self.add_btn, 2, 0, 1, 2)
+        self.grid_layout.addWidget(self.add_btn, 3, 0, 1, 2)
+
+    def fill_systems(self):
+        self.system_name_edit.clear()
+        systems_list = list(SystemM.select(SystemM.name).
+                            where(SystemM.plane_type == self.plane.plane_type,
+                                  SystemM.subunit_type == SubunitM.get(SubunitM.name ==
+                                                                       self.subunit_name_edit.currentText()).id))
+        systems_name = map(lambda q: q.name, systems_list)
+        self.system_name_edit.addItems(systems_name)
+
+    def fill_agr(self):
+        self.agr_select.clear()
+        agrs_list = list(AgregateM.select().where(
+            AgregateM.id_system == SystemM.get(SystemM.name == self.system_name_edit.currentText()).id))
+        agrs_name = map(lambda q: q.name, agrs_list)
+        self.agr_select.addItems(agrs_name)
 
     def add_defect(self):
         pass
